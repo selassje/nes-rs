27443b9821441eeb5e6e8b627dcf52c97059d754
Przemyslaw Koziol <przemkoz85@gmail.com>
2020-09-26 06:19:41 +0200

    adjust test timings

--- tests/instr_timing.rs
+++ tests/instr_timing.rs
@@ -7,10 +7,10 @@ const PATH: &str = "tests\\instr_timing\\";
 
 #[test]
 fn instr_timing() {
-    run_simple_test(PATH, "1-instr_timing.nes",Duration::from_secs(20));
+    run_simple_test(PATH, "1-instr_timing.nes",Duration::from_secs(30));
 }
 
 #[test]
 fn branch_timing() {
-    run_simple_test(PATH, "2-branch_timing.nes",Duration::from_secs(3));
+    run_simple_test(PATH, "2-branch_timing.nes",Duration::from_secs(5));
 }
