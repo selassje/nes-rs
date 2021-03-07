4f7be998f101711f428d3c41c77a390936de5783
Przemyslaw Koziol <przemkoz85@gmail.com>
2021-03-07 02:23:30 +0100

    move guibuilder to separate module

--- src/io/io_sdl2_imgui_opengl.rs
+++ src/io/io_sdl2_imgui_opengl.rs
@@ -1,3 +1,7 @@
+mod gui_builder;
+
+use gui_builder::MenuBarItem;
+
 use std::collections::HashMap;
 use std::default::Default;
 use std::iter::FromIterator;
@@ -7,7 +11,6 @@ use crate::common;
 use crate::io;
 
 use gl::types::*;
-use imgui::im_str;
 use io::SampleFormat;
 
 const SAMPLING_RATE: usize = 44100;
@@ -20,69 +23,6 @@ const DISPLAY_WIDTH: usize = DISPLAY_SCALING * io::FRAME_WIDTH;
 const DISPLAY_HEIGHT: usize = DISPLAY_SCALING * io::FRAME_HEIGHT;
 const MENU_BAR_HEIGHT: usize = 18;
 
-macro_rules! add_font_from_ttf {
-    ($font_path:literal,$size:expr, $imgui:ident) => {{
-        let font_source = imgui::FontSource::TtfData {
-            data: include_bytes!($font_path),
-            size_pixels: $size,
-            config: None,
-        };
-        $imgui.fonts().add_font(&[font_source])
-    }};
-}
-macro_rules! with_font {
-    ($font:expr, $ui:ident, $code:expr) => {{
-        let font_token = $ui.push_font($font);
-        $code
-        font_token.pop($ui);
-    }};
-}
-macro_rules! with_token {
-    ($ui:expr, $token_function:ident, ($($arg:expr),*), $code:expr) => {{
-        if let Some(token) = $ui.$token_function($($arg),*) {
-            $code
-            token.end($ui);
-        }
-    }};
-}
-macro_rules! with_styles {
-    ($ui:expr, ($($style:expr),*), $code:expr) => {{
-        let styles_token = $ui.push_style_vars(&[$($style),*]);
-        $code
-        styles_token.pop($ui);
-}};
-}
-macro_rules! create_simple_window {
-    ($name:tt, $position:expr, $size:expr, $condition_pos:expr, $condition_size:expr) => {{
-        imgui::Window::new(im_str!($name))
-            .scrollable(false)
-            .no_decoration()
-            .position($position, $condition_pos)
-            .size($size, $condition_size)
-    }};
-}
-
-macro_rules! create_unmovable_simple_window {
-    ($name:tt, $position:expr, $size:expr) => {{
-        create_simple_window!(
-            $name,
-            $position,
-            $size,
-            imgui::Condition::Always,
-            imgui::Condition::Always
-        )
-    }};
-}
-
-macro_rules! create_menu_item {
-    ($name:tt, $shortcut:tt) => {{
-        imgui::MenuItem::new(im_str!($name))
-            .selected(false)
-            .enabled(true)
-            .shortcut(im_str!($shortcut))
-    }};
-}
-
 struct SampleBuffer {
     size: usize,
     sum: f32,
@@ -90,28 +30,6 @@ struct SampleBuffer {
     target_bucket_size: f32,
     buffer: [io::SampleFormat; BUFFER_SIZE],
 }
-
-enum GuiFont {
-    _Default = 0,
-    FpsCounter,
-    MenuBar,
-    FontsCount,
-}
-#[derive(PartialEq)]
-enum MenuBarItem {
-    LoadNesFile,
-    Quit,
-    PowerCycle,
-    Pause,
-    SpeedNormal,
-    SpeedDouble,
-    SpeedHalf,
-    SpeedIncrease,
-    SpeedDecrease,
-    None,
-}
-
-type GuiFonts = [imgui::FontId; GuiFont::FontsCount as usize];
 
 impl SampleBuffer {
     fn add(&mut self, sample: io::SampleFormat) {
@@ -149,138 +67,6 @@ struct KeyboardShortCuts {
     speed_decrease: bool,
 }
 
-struct GuiBuilder {
-    emulation_texture: imgui::TextureId,
-    fonts: GuiFonts,
-    menu_bar_item_selected: [bool; MenuBarItem::None as usize],
-    choose_nes_file: bool,
-    rom_path: Option<String>,
-    paused: bool,
-}
-
-impl GuiBuilder {
-    fn update_menu_item_status(&mut self, ui: &mut imgui::Ui, item: MenuBarItem) {
-        self.menu_bar_item_selected[item as usize] = ui.is_item_clicked(imgui::MouseButton::Left)
-            || (ui.is_item_focused() && ui.is_key_pressed(sdl2::keyboard::Scancode::Return as _));
-    }
-
-    fn build_menu_bar_and_check_for_mouse_events(&mut self, fps: u16, ui: &mut imgui::Ui) {
-        use MenuBarItem::*;
-        with_font!(self.fonts[GuiFont::MenuBar as usize], ui, {
-            with_token!(ui, begin_main_menu_bar, (), {
-                with_token!(ui, begin_menu, (im_str!("File"), !self.choose_nes_file), {
-                    create_menu_item!("Load Nes File", "Ctrl + O").build(ui);
-                    self.update_menu_item_status(ui, LoadNesFile);
-
-                    create_menu_item!("Quit", "Esc").build(ui);
-                    self.update_menu_item_status(ui, Quit);
-                });
-            });
-            with_token!(ui, begin_main_menu_bar, (), {
-                with_token!(ui, begin_menu, (im_str!("Emulation"), true), {
-                    create_menu_item!("Power Cycle", "Ctrl + R").build(ui);
-                    self.update_menu_item_status(ui, PowerCycle);
-
-                    create_menu_item!("Pause", "Ctrl + P")
-                        .selected(self.paused)
-                        .build(ui);
-                    self.update_menu_item_status(ui, Pause);
-
-                    let is_speed_selected = |target_fps: u16| fps == target_fps;
-
-                    with_token!(ui, begin_menu, (im_str!("Speed"), true), {
-                        create_menu_item!("Normal", "")
-                            .selected(is_speed_selected(common::DEFAULT_FPS))
-                            .build(ui);
-                        self.update_menu_item_status(ui, SpeedNormal);
-                        create_menu_item!("Double", "")
-                            .selected(is_speed_selected(common::DOUBLE_FPS))
-                            .build(ui);
-                        self.update_menu_item_status(ui, SpeedDouble);
-                        create_menu_item!("Half", "")
-                            .selected(is_speed_selected(common::HALF_FPS))
-                            .build(ui);
-                        self.update_menu_item_status(ui, SpeedHalf);
-                        create_menu_item!("Increase", "Ctrl + =").build(ui);
-                        self.update_menu_item_status(ui, SpeedIncrease);
-                        create_menu_item!("Decrease", "Ctrl + -").build(ui);
-                        self.update_menu_item_status(ui, SpeedDecrease);
-                    });
-                });
-            });
-        });
-    }
-
-    fn build_emulation_window(&self, ui: &mut imgui::Ui) {
-        create_unmovable_simple_window!(
-            "emulation",
-            [0.0, MENU_BAR_HEIGHT as _],
-            [DISPLAY_WIDTH as _, DISPLAY_HEIGHT as _]
-        )
-        .bring_to_front_on_focus(false)
-        .build(ui, || {
-            imgui::Image::new(
-                self.emulation_texture,
-                [DISPLAY_WIDTH as _, DISPLAY_HEIGHT as _],
-            )
-            .build(ui);
-        });
-    }
-
-    fn build_fps_counter(&self, current_fps: u16, target_fps: u16, ui: &mut imgui::Ui) {
-        with_font!(self.fonts[GuiFont::FpsCounter as usize], ui, {
-            let text = format!("FPS {}/{}", current_fps, target_fps);
-            let text_size = ui.calc_text_size(
-                imgui::ImString::new(text.clone()).as_ref(),
-                false,
-                DISPLAY_WIDTH as _,
-            );
-            create_unmovable_simple_window!(
-                "fps",
-                [DISPLAY_WIDTH as f32 - text_size[0], MENU_BAR_HEIGHT as _],
-                text_size
-            )
-            .bg_alpha(0.0)
-            .build(ui, || {
-                ui.text(text);
-            });
-        });
-    }
-
-    fn build_load_nes_file_explorer(&mut self) {
-        let result = nfd::open_file_dialog(None, None).unwrap_or_else(|e| {
-            panic!(e);
-        });
-        match result {
-            nfd::Response::Okay(file_path) => {
-                self.rom_path = Some(file_path);
-            }
-            nfd::Response::Cancel => {}
-            _ => panic!("Unsupported file selection"),
-        }
-    }
-
-    fn build(&mut self, current_fps: u16, target_fps: u16, mut ui: &mut imgui::Ui) {
-        with_styles!(
-            &mut ui,
-            (
-                imgui::StyleVar::WindowRounding(0.0),
-                imgui::StyleVar::WindowBorderSize(0.0),
-                imgui::StyleVar::WindowPadding([0.0, 0.0])
-            ),
-            {
-                self.build_menu_bar_and_check_for_mouse_events(target_fps, &mut ui);
-                self.build_emulation_window(&mut ui);
-                self.build_fps_counter(current_fps, target_fps, &mut ui);
-                if self.choose_nes_file {
-                    self.build_load_nes_file_explorer();
-                    self.paused = false;
-                }
-            }
-        );
-    }
-}
-
 pub struct IOSdl2ImGuiOpenGl {
     io_internal: io_internal::IOInternal,
     sample_buffer: SampleBuffer,
@@ -292,7 +78,7 @@ pub struct IOSdl2ImGuiOpenGl {
     window: sdl2::video::Window,
     renderer: imgui_opengl_renderer::Renderer,
     _gl_context: sdl2::video::GLContext,
-    gui_builder: GuiBuilder,
+    gui_builder: gui_builder::GuiBuilder,
     keyboard_shortcuts: KeyboardShortCuts,
 }
 
@@ -367,9 +153,9 @@ impl IOSdl2ImGuiOpenGl {
             .config_flags
             .set(imgui::ConfigFlags::NAV_ENABLE_KEYBOARD, true);
 
-        let fonts = Self::prepare_fonts(&mut imgui);
-
         let imgui_sdl2 = imgui_sdl2::ImguiSdl2::new(&mut imgui, &window);
+
+        let fonts = gui_builder::prepare_fonts(&mut imgui);
 
         let events = sdl_context.event_pump().unwrap();
         let renderer = imgui_opengl_renderer::Renderer::new(&mut imgui, |s| {
@@ -386,6 +172,8 @@ impl IOSdl2ImGuiOpenGl {
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);
         }
 
+        let gui_builder =
+            gui_builder::GuiBuilder::new(imgui::TextureId::from(emulation_texture as usize), fonts);
         IOSdl2ImGuiOpenGl {
             io_internal: io_internal::IOInternal::new(),
             sample_buffer: SampleBuffer {
@@ -403,63 +191,63 @@ impl IOSdl2ImGuiOpenGl {
             imgui_sdl2,
             renderer,
             _gl_context,
-            gui_builder: GuiBuilder {
-                emulation_texture: imgui::TextureId::from(emulation_texture as usize),
-                fonts,
-                menu_bar_item_selected: Default::default(),
-                choose_nes_file: false,
-                rom_path: None,
-                paused: false,
-            },
+            gui_builder,
             keyboard_shortcuts: Default::default(),
         }
     }
 
-    fn prepare_fonts(imgui: &mut imgui::Context) -> GuiFonts {
-        let default_font = imgui
-            .fonts()
-            .add_font(&[imgui::FontSource::DefaultFontData { config: None }]);
-
-        let mut fonts = [default_font; 3];
-        fonts[GuiFont::FpsCounter as usize] =
-            add_font_from_ttf!("../../res/OpenSans-Regular.ttf", 30.0, imgui);
-
-        fonts[GuiFont::MenuBar as usize] =
-            add_font_from_ttf!("../../res/Roboto-Regular.ttf", 20.0, imgui);
-        fonts
-    }
-
     fn update_io_state(&mut self, io_state: &mut io::IOState) {
         io_state.quit |= self.keyboard_shortcuts.quit
-            || self.gui_builder.menu_bar_item_selected[MenuBarItem::Quit as usize];
+            || self
+                .gui_builder
+                .is_menu_bar_item_selected(MenuBarItem::Quit);
         io_state.power_cycle = self.keyboard_shortcuts.power_cycle
-            || self.gui_builder.menu_bar_item_selected[MenuBarItem::PowerCycle as usize];
+            || self
+                .gui_builder
+                .is_menu_bar_item_selected(MenuBarItem::PowerCycle);
         self.gui_builder.choose_nes_file = self.keyboard_shortcuts.load_nes_file
-            || self.gui_builder.menu_bar_item_selected[MenuBarItem::LoadNesFile as usize];
+            || self
+                .gui_builder
+                .is_menu_bar_item_selected(MenuBarItem::LoadNesFile);
 
         io_state.speed = None;
         if self.keyboard_shortcuts.speed_increase
-            || self.gui_builder.menu_bar_item_selected[MenuBarItem::SpeedIncrease as usize]
+            || self
+                .gui_builder
+                .is_menu_bar_item_selected(MenuBarItem::SpeedIncrease)
         {
             io_state.speed = Some(io::Speed::Increase);
         }
         if self.keyboard_shortcuts.speed_decrease
-            || self.gui_builder.menu_bar_item_selected[MenuBarItem::SpeedDecrease as usize]
+            || self
+                .gui_builder
+                .is_menu_bar_item_selected(MenuBarItem::SpeedDecrease)
         {
             io_state.speed = Some(io::Speed::Decrease);
         }
-        if self.gui_builder.menu_bar_item_selected[MenuBarItem::SpeedNormal as usize] {
+        if self
+            .gui_builder
+            .is_menu_bar_item_selected(MenuBarItem::SpeedNormal)
+        {
             io_state.speed = Some(io::Speed::Normal)
         }
-        if self.gui_builder.menu_bar_item_selected[MenuBarItem::SpeedDouble as usize] {
+        if self
+            .gui_builder
+            .is_menu_bar_item_selected(MenuBarItem::SpeedDouble)
+        {
             io_state.speed = Some(io::Speed::Double)
         }
-        if self.gui_builder.menu_bar_item_selected[MenuBarItem::SpeedHalf as usize] {
+        if self
+            .gui_builder
+            .is_menu_bar_item_selected(MenuBarItem::SpeedHalf)
+        {
             io_state.speed = Some(io::Speed::Half)
         }
 
         if self.keyboard_shortcuts.pause
-            || self.gui_builder.menu_bar_item_selected[MenuBarItem::Pause as usize]
+            || self
+                .gui_builder
+                .is_menu_bar_item_selected(MenuBarItem::Pause)
         {
             io_state.pause = !self.gui_builder.paused;
         } else {
@@ -499,9 +287,7 @@ impl IOSdl2ImGuiOpenGl {
 impl io::IO for IOSdl2ImGuiOpenGl {
     fn present_frame(&mut self, control: io::IOControl) -> io::IOState {
         let mut io_state: io::IOState = Default::default();
-        self.gui_builder.menu_bar_item_selected = Default::default();
-        self.gui_builder.rom_path = None;
-        self.gui_builder.paused = control.pause;
+        self.gui_builder.prepare_for_new_frame(control.pause);
         self.keyboard_shortcuts = Default::default();
 
         self.keyboard_state = HashMap::from_iter(self.events.keyboard_state().scancodes());
@@ -550,6 +336,7 @@ impl io::IO for IOSdl2ImGuiOpenGl {
         );
 
         let mut ui = self.imgui.frame();
+        //ui.show_demo_window(&mut true);
         self.imgui_sdl2.prepare_render(&ui, &self.window);
 
         self.gui_builder
---
+++ src/io/io_sdl2_imgui_opengl/gui_builder.rs
@@ -0,0 +1,255 @@
+use imgui::im_str;
+
+use super::{DISPLAY_HEIGHT, DISPLAY_WIDTH, MENU_BAR_HEIGHT};
+use crate::common;
+
+macro_rules! add_font_from_ttf {
+    ($font_path:literal,$size:expr, $imgui:ident) => {{
+        let font_source = imgui::FontSource::TtfData {
+            data: include_bytes!($font_path),
+            size_pixels: $size,
+            config: None,
+        };
+        $imgui.fonts().add_font(&[font_source])
+    }};
+}
+
+macro_rules! with_font {
+    ($font:expr, $ui:ident, $code:expr) => {{
+        let font_token = $ui.push_font($font);
+        $code
+        font_token.pop($ui);
+    }};
+}
+macro_rules! with_token {
+    ($ui:expr, $token_function:ident, ($($arg:expr),*), $code:expr) => {{
+        if let Some(token) = $ui.$token_function($($arg),*) {
+            $code
+            token.end($ui);
+        }
+    }};
+}
+macro_rules! with_styles {
+    ($ui:expr, ($($style:expr),*), $code:expr) => {{
+        let styles_token = $ui.push_style_vars(&[$($style),*]);
+        $code
+        styles_token.pop($ui);
+}};
+}
+macro_rules! create_simple_window {
+    ($name:tt, $position:expr, $size:expr, $condition_pos:expr, $condition_size:expr) => {{
+        imgui::Window::new(im_str!($name))
+            .scrollable(false)
+            .no_decoration()
+            .position($position, $condition_pos)
+            .size($size, $condition_size)
+    }};
+}
+
+macro_rules! create_unmovable_simple_window {
+    ($name:tt, $position:expr, $size:expr) => {{
+        create_simple_window!(
+            $name,
+            $position,
+            $size,
+            imgui::Condition::Always,
+            imgui::Condition::Always
+        )
+    }};
+}
+
+macro_rules! create_menu_item {
+    ($name:tt, $shortcut:tt) => {{
+        imgui::MenuItem::new(im_str!($name))
+            .selected(false)
+            .enabled(true)
+            .shortcut(im_str!($shortcut))
+    }};
+}
+#[derive(PartialEq)]
+pub(super) enum MenuBarItem {
+    LoadNesFile,
+    Quit,
+    PowerCycle,
+    Pause,
+    SpeedNormal,
+    SpeedDouble,
+    SpeedHalf,
+    SpeedIncrease,
+    SpeedDecrease,
+    None,
+}
+enum GuiFont {
+    _Default = 0,
+    FpsCounter,
+    MenuBar,
+    FontsCount,
+}
+
+type GuiFonts = [imgui::FontId; GuiFont::FontsCount as usize];
+
+pub(super) fn prepare_fonts(imgui: &mut imgui::Context) -> GuiFonts {
+    let default_font = imgui
+        .fonts()
+        .add_font(&[imgui::FontSource::DefaultFontData { config: None }]);
+
+    let mut fonts = [default_font; 3];
+    fonts[GuiFont::FpsCounter as usize] =
+        add_font_from_ttf!("../../../res/OpenSans-Regular.ttf", 30.0, imgui);
+
+    fonts[GuiFont::MenuBar as usize] =
+        add_font_from_ttf!("../../../res/Roboto-Regular.ttf", 20.0, imgui);
+    fonts
+}
+pub(super) struct GuiBuilder {
+    emulation_texture: imgui::TextureId,
+    fonts: GuiFonts,
+    menu_bar_item_selected: [bool; MenuBarItem::None as usize],
+    pub choose_nes_file: bool,
+    pub rom_path: Option<String>,
+    pub paused: bool,
+}
+
+impl GuiBuilder {
+    pub(super) fn new(emulation_texture: imgui::TextureId, fonts: GuiFonts) -> Self {
+        Self {
+            emulation_texture,
+            menu_bar_item_selected: Default::default(),
+            fonts,
+            choose_nes_file: false,
+            rom_path: None,
+            paused: false,
+        }
+    }
+
+    pub(super) fn prepare_for_new_frame(&mut self, paused: bool) {
+        self.menu_bar_item_selected = Default::default();
+        self.rom_path = None;
+        self.paused = paused;
+    }
+
+    pub(super) fn is_menu_bar_item_selected(&self, item: MenuBarItem) -> bool {
+        self.menu_bar_item_selected[item as usize]
+    }
+
+    fn update_menu_item_status(&mut self, ui: &mut imgui::Ui, item: MenuBarItem) {
+        self.menu_bar_item_selected[item as usize] = ui.is_item_clicked(imgui::MouseButton::Left)
+            || (ui.is_item_focused() && ui.is_key_pressed(sdl2::keyboard::Scancode::Return as _));
+    }
+
+    fn build_menu_bar_and_check_for_mouse_events(&mut self, fps: u16, ui: &mut imgui::Ui) {
+        use MenuBarItem::*;
+        with_font!(self.fonts[GuiFont::MenuBar as usize], ui, {
+            with_token!(ui, begin_main_menu_bar, (), {
+                with_token!(ui, begin_menu, (im_str!("File"), !self.choose_nes_file), {
+                    create_menu_item!("Load Nes File", "Ctrl + O").build(ui);
+                    self.update_menu_item_status(ui, LoadNesFile);
+
+                    create_menu_item!("Quit", "Esc").build(ui);
+                    self.update_menu_item_status(ui, Quit);
+                });
+            });
+            with_token!(ui, begin_main_menu_bar, (), {
+                with_token!(ui, begin_menu, (im_str!("Emulation"), true), {
+                    create_menu_item!("Power Cycle", "Ctrl + R").build(ui);
+                    self.update_menu_item_status(ui, PowerCycle);
+
+                    create_menu_item!("Pause", "Ctrl + P")
+                        .selected(self.paused)
+                        .build(ui);
+                    self.update_menu_item_status(ui, Pause);
+
+                    let is_speed_selected = |target_fps: u16| fps == target_fps;
+
+                    with_token!(ui, begin_menu, (im_str!("Speed"), true), {
+                        create_menu_item!("Normal", "")
+                            .selected(is_speed_selected(common::DEFAULT_FPS))
+                            .build(ui);
+                        self.update_menu_item_status(ui, SpeedNormal);
+                        create_menu_item!("Double", "")
+                            .selected(is_speed_selected(common::DOUBLE_FPS))
+                            .build(ui);
+                        self.update_menu_item_status(ui, SpeedDouble);
+                        create_menu_item!("Half", "")
+                            .selected(is_speed_selected(common::HALF_FPS))
+                            .build(ui);
+                        self.update_menu_item_status(ui, SpeedHalf);
+                        create_menu_item!("Increase", "Ctrl + =").build(ui);
+                        self.update_menu_item_status(ui, SpeedIncrease);
+                        create_menu_item!("Decrease", "Ctrl + -").build(ui);
+                        self.update_menu_item_status(ui, SpeedDecrease);
+                    });
+                });
+            });
+        });
+    }
+
+    fn build_emulation_window(&self, ui: &mut imgui::Ui) {
+        create_unmovable_simple_window!(
+            "emulation",
+            [0.0, MENU_BAR_HEIGHT as _],
+            [DISPLAY_WIDTH as _, DISPLAY_HEIGHT as _]
+        )
+        .bring_to_front_on_focus(false)
+        .build(ui, || {
+            imgui::Image::new(
+                self.emulation_texture,
+                [DISPLAY_WIDTH as _, DISPLAY_HEIGHT as _],
+            )
+            .build(ui);
+        });
+    }
+
+    fn build_fps_counter(&self, current_fps: u16, target_fps: u16, ui: &mut imgui::Ui) {
+        with_font!(self.fonts[GuiFont::FpsCounter as usize], ui, {
+            let text = format!("FPS {}/{}", current_fps, target_fps);
+            let text_size = ui.calc_text_size(
+                imgui::ImString::new(text.clone()).as_ref(),
+                false,
+                DISPLAY_WIDTH as _,
+            );
+            create_unmovable_simple_window!(
+                "fps",
+                [DISPLAY_WIDTH as f32 - text_size[0], MENU_BAR_HEIGHT as _],
+                text_size
+            )
+            .bg_alpha(0.0)
+            .build(ui, || {
+                ui.text(text);
+            });
+        });
+    }
+
+    fn build_load_nes_file_explorer(&mut self) {
+        let result = nfd::open_file_dialog(None, None).unwrap_or_else(|e| {
+            panic!(e);
+        });
+        match result {
+            nfd::Response::Okay(file_path) => {
+                self.rom_path = Some(file_path);
+            }
+            nfd::Response::Cancel => {}
+            _ => panic!("Unsupported file selection"),
+        }
+    }
+
+    pub(super) fn build(&mut self, current_fps: u16, target_fps: u16, mut ui: &mut imgui::Ui) {
+        with_styles!(
+            &mut ui,
+            (
+                imgui::StyleVar::WindowRounding(0.0),
+                imgui::StyleVar::WindowBorderSize(0.0),
+                imgui::StyleVar::WindowPadding([0.0, 0.0])
+            ),
+            {
+                self.build_menu_bar_and_check_for_mouse_events(target_fps, &mut ui);
+                self.build_emulation_window(&mut ui);
+                self.build_fps_counter(current_fps, target_fps, &mut ui);
+                if self.choose_nes_file {
+                    self.build_load_nes_file_explorer();
+                    self.paused = false;
+                }
+            }
+        );
+    }
+}
