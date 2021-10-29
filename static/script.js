c754352f6880c220297bcb2e3fd336f837adc979
Przemyslaw Koziol <przemkoz85@gmail.com>
2021-10-29 05:38:28 +0200

    add upload save state

--- static/script.js
+++ static/script.js
@@ -1,19 +1,33 @@
 function upload_nes_file_btn_click() {
     document.getElementById("upload_nes_file").click()
 }
+function upload_save_file_btn_click() {
+    document.getElementById("upload_save_file").click()
+}
 
-function upload_nes_file() {
-    let nes_file = this.files[0];
+function upload_file(file, dir) {
     var reader = new FileReader();
-    reader.readAsArrayBuffer(nes_file);
+    reader.readAsArrayBuffer(file);
     reader.onload = function (evt) {
         var buf = new Uint8Array(evt.target.result);
-        FS.writeFile('games/' + nes_file.name, buf);
+        FS.writeFile(dir + "/" + file.name, buf);
     }
 }
+
+function upload_nes_file() {
+    upload_file(this.files[0], "games");
+}
+
+function upload_save_file() {
+    upload_file(this.files[0], "saves");
+}
+
 
 FS.rmdir("home/web_user");
 FS.rmdir("home");
 FS.rmdir("tmp");
-FS.mkdir("games")
+FS.mkdir("games");
+FS.mkdir("saves");
+
 document.getElementById("upload_nes_file").addEventListener("change", upload_nes_file, false);
+document.getElementById("upload_save_file").addEventListener("change", upload_save_file, false);
